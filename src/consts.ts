36427c5d8f812d9b64f4b1d288b3c25fe9e0307e
siprianushondro-1982 <siprianus.hondro42@guru.smk.belajar.id>
2025-05-04 14:30:46 +0700

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "Portal Edukasi";
+export const SITE_DESCRIPTION = "Selamat Datang di Halaman Portal Edukasi!";
